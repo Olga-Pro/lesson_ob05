7b7a0acf6ca4806da3e46949dcc72afa33743092
Olga-Pro <oip_@inbox.ru>
2024-04-12 10:35:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ pygame.init()
 # Константы
 
 # Настройки игрового окна
-SCREEN_WIDTH, SCREEN_HEIGTH = 300, 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+SCREEN_WIDTH, SCREEN_HEIGHT = 300, 600
 
 # Параметры игрового поля
 COLUMNS, ROWS = 10, 20
@@ -23,16 +22,9 @@ gray = (128, 128, 128)
 colors = [(0, 255, 255), (255, 165, 0), (0, 0, 255), (255, 255, 0),
           (128, 0, 128), (0, 255, 0), (255, 0, 0), gray]
 
-# Настройки игровых параметров
-clock = pygame.time.Clock()
-fps = 5
-
-
-# Инициализация игрового поля
-board = [[black for _ in range(COLUMNS)] for _ in range(ROWS)]
 
 # Описываем формы фигур
-shapes = [
+SHAPES = [
     [[1, 1, 1],
      [0, 1, 0]],
 
@@ -60,7 +52,7 @@ class Piece(object):
         self.x = column
         self.y = row
         self.shape = shape
-        self.color = colors[shapes.index(shape)]
+        self.color = colors[SHAPES.index(shape)]
 
     def rotate_shape(self):
         # повернуть фигуру
@@ -87,7 +79,7 @@ class Piece(object):
         else:
             return False
 
-    def valid_space(self, l_r):
+    def valid_space(self, Board, l_r):
         # проверить пространство для фигуры на игровом поле
         x_shape = self.x // CELL_SIZE + l_r
         y_shape = self.y // CELL_SIZE + 1
@@ -97,11 +89,11 @@ class Piece(object):
                 if block != 0:  # не рисовать пустые квадратики
                     if y_shape + yy >= ROWS or x_shape + xx >= COLUMNS:
                         return False
-                    elif board[y_shape + yy][x_shape + xx] != (0, 0, 0):
+                    elif Board.board[y_shape + yy][x_shape + xx] != (0, 0, 0):
                         return False
         return True
 
-    def save_on_board(self, x, y):
+    def save_on_board(self, Board, x, y):
         # сохранить фигуру на игровом поле
         x_shape = x // CELL_SIZE
         y_shape = y // CELL_SIZE
@@ -109,56 +101,78 @@ class Piece(object):
         for yy, row in enumerate(self.shape):
             for xx, block in enumerate(row):
                 if block != 0:  # не рисовать пустые квадратики
-                    board[y_shape+yy][x_shape+xx] = self.color
+                    Board.board[y_shape+yy][x_shape+xx] = self.color
+
+class Board():
+    def __init__(self):
+        # Инициализация игрового поля
+        self.board = [[black for _ in range(COLUMNS)] for _ in range(ROWS)]
+
+    # Функция для отрисовки экрана и сетки
+    def draw_board(self, surface):
+        for y in range(ROWS):
+            for x in range(COLUMNS):
+                rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                pygame.draw.rect(surface, self.board[y][x], rect)
+                pygame.draw.rect(surface, gray, rect, 1)
+
+    # Функция отрисовки одного квадратика из фигуры
+    def draw_block(self, x, y, color, screen):
+        pygame.draw.rect(screen, color, (x, y, CELL_SIZE, CELL_SIZE))
+
+    # Функция отрисовки фигуры
+    def draw_shape(self, x, y, piece, screen):
+        for yy, row in enumerate(piece.shape):
+            for xx, block in enumerate(row):
+                if block != 0:  # не рисовать пустые квадратики
+                    self.draw_block(x + xx * CELL_SIZE, y + yy * CELL_SIZE, piece.color, screen)
+
+    def remove_row(self, num_row):
+        # удаление строки -> сдвинуть все строки вниз
+        for i in range(num_row, 1, -1):
+            for j in range(0, COLUMNS - 1):
+                self.board[i][j] = self.board[i - 1][j]
+        for j in range(0, COLUMNS - 1):  # очистить верхнюю строку
+            self.board[0][j] = (0, 0, 0)
+
+    def full_row(self):
+        # проверка заполненности строки и удаление заполненной
+        for yy, row in enumerate(self.board):
+            res = 0
+            for xx, block in enumerate(row):
+                if self.board[yy][xx] != (0, 0, 0):
+                    res += 1
+            if res == COLUMNS:
+                self.remove_row(yy)
+
+
+class Game():
+    def __init__(self):
+        self.board = Board()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    def start(self):
+        pass
+
 
 
 
 
 def create_piece():
-    return Piece(0, 0, random.choice(shapes))
-
-# Функция отрисовки одного квадратика из фигуры
-def draw_block(x, y, color):
-    pygame.draw.rect(screen, color, (x, y, CELL_SIZE, CELL_SIZE))
-
-# Функция отрисовки фигуры
-def draw_shape(x, y, piece):
-    for yy, row in enumerate(piece.shape):
-        for xx, block in enumerate(row):
-            if block != 0: # не рисовать пустые квадратики
-                draw_block(x + xx * CELL_SIZE, y + yy * CELL_SIZE, piece.color)
-
-# Функция для отрисовки экрана и сетки
-def draw_board(surface):
-    for y in range(ROWS):
-        for x in range(COLUMNS):
-            rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-            pygame.draw.rect(surface, board[y][x], rect)
-            pygame.draw.rect(surface, gray, rect, 1)
-
-def remove_row(num_row):
-    for i in range(num_row, 1, -1):  # удаление строки -> сдвинуть все строки вниз
-        for j in range(0, COLUMNS - 1):
-            board[i][j] = board[i - 1][j]
-    for j in range(0, COLUMNS - 1):  # очистить верхнюю строку
-        board[0][j] = (0, 0, 0)
-
-def full_row():
-    # проверка заполненности строки и удаление заполненной
-    for yy, row in enumerate(board):
-        res = 0
-        for xx, block in enumerate(row):
-            if board[yy][xx] != (0, 0, 0):
-                res += 1
-        if res == COLUMNS:
-            remove_row(yy)
+    return Piece(0, 0, random.choice(SHAPES))
 
 
-
+game = Game()
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Настройки игровых параметров
+clock = pygame.time.Clock()
+fps = 5
 
 pygame.display.set_caption('Тетрис')
 # Основной игровой цикл
 def main():
+    # game = Game()
+    # screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     run = True
     current_piece = create_piece()
 
@@ -170,41 +184,40 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     # Переместить фигуру влево
-                    if not current_piece.on_left_board() and not current_piece.on_bottom() and current_piece.valid_space(-1):
+                    if not current_piece.on_left_board() and not current_piece.on_bottom() and current_piece.valid_space(game.board, -1):
                         current_piece.x -= CELL_SIZE
                 elif event.key == pygame.K_RIGHT:
                     # Переместить фигуру вправо
-                    if not current_piece.on_right_board() and not current_piece.on_bottom() and current_piece.valid_space(1):
+                    if not current_piece.on_right_board() and not current_piece.on_bottom() and current_piece.valid_space(game.board, 1):
                         current_piece.x += CELL_SIZE
                 elif event.key == pygame.K_DOWN:
                     # Ускорить падение фигуры
-                    while current_piece.valid_space(0) and not current_piece.on_bottom():
+                    while current_piece.valid_space(game.board, 0) and not current_piece.on_bottom():
                         # пока есть пространство - двигаем вниз
                         current_piece.y += CELL_SIZE
                 elif event.key == pygame.K_UP:
                     # Повернуть фигуру
                     rotate_piece = current_piece
                     rotate_piece.rotate_shape()
-                    if rotate_piece.valid_space(0):
+                    if rotate_piece.valid_space(game.board, 0):
                         current_piece = rotate_piece
 
+        game.board.draw_board(game.screen)
+        game.board.draw_shape(current_piece.x, current_piece.y, current_piece, game.screen)
+        game.board.full_row()
 
-        draw_board(screen)
-        draw_shape(current_piece.x, current_piece.y, current_piece)
-        full_row()
-
-        if not current_piece.on_bottom() and current_piece.valid_space(0):
+        if not current_piece.on_bottom() and current_piece.valid_space(game.board, 0):
             # если не дно и есть пространство - едем вниз
             current_piece.y += CELL_SIZE
         else:
-            current_piece.save_on_board(current_piece.x, current_piece.y)
+            current_piece.save_on_board(game.board, current_piece.x, current_piece.y)
             current_piece = create_piece()
             # проверить заполненность игрового поля
-            if not current_piece.valid_space(0):
+            if not current_piece.valid_space(game.board, 0):
                 print("Игра окончена")
                 run = False
 
-        print(f"{current_piece.x} - {current_piece.y}")
+        #print(f"{current_piece.x} - {current_piece.y}")
         pygame.display.flip()
         clock.tick(fps)
 
